ad2dbc93b669b4717015096e43a5c49141c02dcc
Mohit Vellanki <mohit.vellanki2593@gmail.com>
2017-06-13 22:34:00 +0200

    safety first

---
+++ a2.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  7 18:16:33 2017
+
+@author: mohit
+"""
+
+#all the imports to be imported.
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import StratifiedKFold
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import statistics
+acc1 = []
+acc2 = []
+acc3 = []
+columns = ['word_freq_make', 'word_freq_address', 'word_freq_all', 'word_freq_3d', 'word_freq_our', 'word_freq_over', 'word_freq_remove', 'word_freq_internet', 'word_freq_order', 'word_freq_mail', 'word_freq_receive', 'word_freq_will', 'word_freq_people', 'word_freq_report', 'word_freq_addresses', 'word_freq_free', 'word_freq_business', 'word_freq_email', 'word_freq_you', 'word_freq_credit', 'word_freq_your', 'word_freq_font', 'word_freq_000', 'word_freq_money', 'word_freq_hp', 'word_freq_hpl', 'word_freq_george', 'word_freq_650', 'word_freq_lab', 'word_freq_labs', 'word_freq_telnet', 'word_freq_857', 'word_freq_data', 'word_freq_415', 'word_freq_85', 'word_freq_technology', 'word_freq_1999', 'word_freq_parts', 'word_freq_pm', 'word_freq_direct', 'word_freq_cs', 'word_freq_meeting', 'word_freq_original', 'word_freq_project', 'word_freq_re', 'word_freq_edu', 'word_freq_table', 'word_freq_conference', 'char_freq_;', 'char_freq_(', 'char_freq_[', 'char_freq_!', 'char_freq_$', 'char_freq_#', 'capital_run_length_average', 'capital_run_length_longest', 'capital_run_length_total', 'spam']
+data = pd.read_csv("/home/mohit/projects/bth/ML/a2/spambase/spambase.data",  delimiter=",",names=columns)
+X = data.iloc[:,0:57]
+y = data['spam']
+
+clf_NB = GaussianNB()
+clf_DT = DecisionTreeClassifier()
+clf_LR = LogisticRegression()
+
+skf = StratifiedKFold(n_splits=10)
+skf.get_n_splits(X,y)
+print(skf)
+print("______________________________________________________")
+print("Folds\t\tNB\tDT\tLR")
+print("______________________________________________________")
+i=1    
+for train_index, test_index in skf.split(X,y):
+    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    
+    clf_NB.fit(X_train, y_train)
+    pred = clf_NB.predict(X_test)
+    a = round(accuracy_score(y_test, pred),3)
+    acc1.append(a)
+    
+    clf_DT.fit(X_train, y_train)
+    pred = clf_DT.predict(X_test)
+    b = round(accuracy_score(y_test, pred),3)
+    acc2.append(b)
+    
+    clf_LR.fit(X_train, y_train)
+    pred = clf_LR.predict(X_test)
+    c = round(accuracy_score(y_test, pred),3)
+    acc3.append(c)
+    print(i,a,"\t","\t",b,"\t",c)
+    
+arr = np.array([acc1,acc2,acc3])
+avg1 =  statistics.mean(acc1)
+print(avg1)
