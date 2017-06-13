da0d9c64647cb4e51ab470cf33de742c71e0402d
Mohit Vellanki <mohit.vellanki2593@gmail.com>
2017-06-14 00:12:26 +0200

    cv, time, fscore done

--- a2.py
+++ a2.py
@@ -7,14 +7,16 @@ Created on Wed Jun  7 18:16:33 2017
 """
 
 #all the imports to be imported.
-import numpy as np
 import pandas as pd
 from sklearn.model_selection import StratifiedKFold
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score
 import statistics
+from time import clock
+from scipy.stats import friedmanchisquare as fmcs
+from stac import nonparametric_tests
 acc1 = []
 acc2 = []
 acc3 = []
@@ -30,30 +32,44 @@ clf_LR = LogisticRegression()
 skf = StratifiedKFold(n_splits=10)
 skf.get_n_splits(X,y)
 print(skf)
-print("______________________________________________________")
+print("________________________________________________")
 print("Folds\t\tNB\tDT\tLR")
-print("______________________________________________________")
+print("________________________________________________")
 i=1    
 for train_index, test_index in skf.split(X,y):
     X_train, X_test = X.iloc[train_index], X.iloc[test_index]
     y_train, y_test = y[train_index], y[test_index]
     
+    start_time = clock()
     clf_NB.fit(X_train, y_train)
+    time_NB = round(clock()-start_time,3)
     pred = clf_NB.predict(X_test)
     a = round(accuracy_score(y_test, pred),3)
     acc1.append(a)
+    fm_NB = f1_score(y_test, pred)
     
+    start_time = clock()
     clf_DT.fit(X_train, y_train)
+    time_DT = round(clock() - start_time,3)
     pred = clf_DT.predict(X_test)
     b = round(accuracy_score(y_test, pred),3)
     acc2.append(b)
+    fm_DT = f1_score(y_test, pred)
     
+    start_time = clock()
     clf_LR.fit(X_train, y_train)
+    time_LR = (clock() - start_time,3)
     pred = clf_LR.predict(X_test)
+    fm_DT = f1_score(y_test, pred)
     c = round(accuracy_score(y_test, pred),3)
     acc3.append(c)
-    print(i,a,"\t","\t",b,"\t",c)
-    
-arr = np.array([acc1,acc2,acc3])
-avg1 =  statistics.mean(acc1)
-print(avg1)
+    fm_LR = f1_score(y_test, pred)
+
+    print(i,"\t\t",a,"\t",b,"\t",c)
+    i=i+1
+print("______________________________________________________")
+print("avg","\t","\t",statistics.mean(acc1),statistics.mean(acc2),"\t",statistics.mean(acc3))
+print("stddev","\t","\t",statistics.stdev(acc1),statistics.stdev(acc2),"\t",statistics.stdev(acc3))
+print("time",time_NB,"",time_DT,"", time_LR)
+print("f1scores",fm_NB,"",fm_DT,"",fm_LR)
+#nonparametric_tests.friedman_test(y_test,pred)
